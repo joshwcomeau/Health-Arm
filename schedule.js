76b6c4cfdf97eb89ea051a753e03f31d8d5ea79e
Zak Kain <zakkain@gmail.com>
2016-05-28 16:49:57 -0400

    add some logic to the drink suggestion time

--- schedule.js
+++ schedule.js
@@ -6,7 +6,18 @@ module.exports = {
     const target = waterGoal.goal.goal;
     const percentageOfGoal = currentAmountDrank / target * 100;
     const percentageOfDay = moment().hour() / 24 * 100;
+    const timeToDrink = 15;
 
-    return moment().add(14, 'minutes').fromNow();
+
+    // always suggest a drink IMMEDIATELY if you're behind drinking water for the time of day
+    // will require changes to the copy on the front end (probably)
+    // otherwise, suggest a drink in an hour
+    if (percentageOfGoal <= percentageOfDay) {
+    	timeToDrink = 1;
+    } else {
+    	timeToDrink = 60;
+    }
+
+    return moment().add(timeToDrink, 'minutes').fromNow();
   }
 }
