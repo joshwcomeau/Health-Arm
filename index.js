0069bbc86bda71a9f62f02d9fe72d102ef555bf8
Josh Comeau <josh@breather.com>
2016-05-28 14:28:46 -0400

    Finish fetching info from API

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
-const path = require("path");
-const app = express();
-const request = require('request');
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
-const { CLIENT_ID, CLIENT_SECRET } = require('./secrets.js')
+
+const { authenticate, profileInfo, waterInfo } = require('./fitbit_api');
+
+const app = express();
 
 
 app.use(express.static('client'));
@@ -13,14 +14,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-
 app.get('/fitbit', async (function(req, res) {
-  console.log("QUERY", req.query)
+  const authInfo = await (authenticate(req.query));
 
-  const fitbitTokenData = await (requestAccessToken(req.query));
+  const profile = await (profileInfo(authInfo));
+  const water = await (waterInfo(authInfo));
 
-  // We want to immediately query and see what their current water logs are
-  console.log("TOKEN DATA", fitbitTokenData);
+  console.log("Got profile", profile);
+  console.log("Water", water)
+
+
+
 }));
 
 app.post('/fire', (req, res) => {
@@ -28,36 +32,5 @@ app.post('/fire', (req, res) => {
 
   res.json({ fired: true })
 });
-
-function requestAccessToken({ code }) {
-  // We need to create a Base64-encoded string of the auth code
-  // with our client secret.
-  const stringToEncode = [CLIENT_ID, CLIENT_SECRET].join(':');
-
-  const encodedString = new Buffer(stringToEncode).toString('base64')
-
-  return new Promise((resolve, reject) => {
-    // Exchange the auth code for an access token
-    request({
-      url: 'https://api.fitbit.com/oauth2/token',
-      method: 'post',
-      form: {
-        code: code,
-        grant_type: 'authorization_code',
-        client_id: CLIENT_ID
-      },
-      headers: {
-        'Authorization': `Basic ${encodedString}`,
-        'Content-Type': 'application/x-www-form-encoded'
-      }
-    }, (err, res) => {
-      console.log("Request done", err, res);
-      if (err) return reject(err);
-      return resolve(res.body);
-    })
-
-  })
-}
-
 
 app.listen(3000);
